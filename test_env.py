8113224ac15d2033b45301ce57e31e9b949ed843
Mehdi Lahrach <mehdi.lahrach@dauphine.eu>
2026-04-13 23:51:45 +0200

    Initial commit: project structure, venv config and requirements

--- test_env.py
+++ test_env.py
@@ -1,6 +1,13 @@
+""" 
+Ce script sert à valider ton environnement de travail.
+
+En gros : “Est-ce que tout est bien installé et prêt pour exécuter le projet ?
+"""
+
+
 import sys
 import numpy as np
 
 print("Python version:", sys.version)
 print("NumPy version:", np.__version__)
-print("Tout fonctionne ! ✅")
+print("Tout fonctionne !")
